2b81bb93091175c12abb3f40669fcbfd269e999e
pranshu9179 <0112ca221094@gmail.com>
2026-01-21 15:36:47 +0530

    utils code update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 // require('dotenv').config({path:'./env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path :"./env"
-})
+  path: "./env",
+});
 
-connectDB ()
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000);
+    console.log(`Server is running at port : ${process.env.PORT} `);
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !!");
+  });
 
 
 
@@ -34,6 +32,12 @@ connectDB ()
 
 
 
+
+
+
+
+
+  
 /*
 import express from "express";
 const app = express();
